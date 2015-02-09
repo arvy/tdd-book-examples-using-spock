55f37d884217cb1e7da3f3c95c0f1e4e29f3bb33
arvydas.jonusonis <arvydas.jonusonis@reachlocal.com>
2015-02-08 21:22:20 -0800

    End chapter 1.

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,13 +3,13 @@
  */
 public class Dollar {
 
-    int amount=10;
+    int amount;
 
     Dollar(int amount){
-
+        this.amount = amount;
     }
 
     void times(int multiplier){
-
+        amount *= multiplier;
     }
 }
