f1a1776ec023d9844acd19e09d0852c0fe5bd0bc
Arvydas Jonusonis <arvydas@gogii.net>
2015-03-13 21:40:55 -0700

    Got rid of Dollar and Franch classes (replacing them with Money). Removed redunant tests.

--- src/test/groovy/DollarSpec.groovy
+++ src/test/groovy/DollarSpec.groovy
@@ -20,19 +20,7 @@ class DollarSpec extends Specification {
         Money.dollar(5) ==  Money.dollar(5)
         Money.dollar(5) != Money.dollar(6)
 
-        Money.franc(5) == Money.franc(5)
-        Money.franc(5) != Money.franc(6)
-
         Money.franc(5) != Money.dollar(5)
-    }
-
-    def "test Franc multiplication"(){
-        given:
-        Franc five = Money.franc(5)
-
-        expect:
-        five.times(2) == Money.franc(10)
-        five.times(3) == Money.franc(15)
     }
 
     def "test currency"(){
@@ -41,8 +29,4 @@ class DollarSpec extends Specification {
         Money.franc(1).currency() == "CHF"
     }
 
-    def "different class equality"(){
-        expect:
-        new Money(10, "CHF") == new Franc(10, "CHF")
-    }
 }
